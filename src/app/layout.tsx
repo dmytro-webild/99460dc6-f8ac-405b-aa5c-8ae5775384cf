60c46dc600bac22dffff873c2a1dc68712873401
bender <kudin.dima@gmail.com>
2026-06-05 01:07:42 +0000

    Update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "@/lib/gsap-setup";
 import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
 import { getVisualEditScript } from "@/utils/visual-edit-script";
 import { Archivo } from "next/font/google";
 
@@ -54,7 +55,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
         <body className={`${archivo.variable} antialiased`}>
-          
+          <Tag />
           {children}
           <script
               dangerouslySetInnerHTML={{
